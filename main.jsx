7709e8a3e78ad1d62061bcda12b08b4887b69330
vikas <vikasin2005@gmail.com>
2025-03-29 19:34:43 +0530

    main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import LoginForm from './logn.jsx'
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <LoginForm/>
-//   </StrictMode>,
-// )
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
