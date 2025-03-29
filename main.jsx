1db02d13b2379bd47bf47512b2392bb2730f80ba
vikas <vikasin2005@gmail.com>
2025-03-29 19:41:11 +0530

    main.jsx

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
 //import App from './App'
-import LoginForm from './logn'
+import LoginForm from './login'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LoginForm />
